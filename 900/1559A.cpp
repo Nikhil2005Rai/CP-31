9687fc51ea8365fc19bd840b78aea3b3069174c6
Nikhil2005Rai <n2005rai@gmail.com>
2026-01-28 16:40:13 +0530

    done more qn

--- 900/1559A.cpp
+++ 900/1559A.cpp
@@ -3,7 +3,13 @@
 using namespace std;
 
 void solve() {
-    
+    int n; cin >> n;
+    vector<int> v(n);
+    for(int &x: v) cin >> x;
+
+    int all_and = v[0];
+    for(int i = 0; i < n; i++) all_and &= v[i];
+    cout << all_and << endl;
 }
 
 int main() {
